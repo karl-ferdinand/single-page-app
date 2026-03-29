1dcbb32204e1e5691f35eee4a17fd90bd4a234a8
Karl Ferdinand <28285671+karl-ferdinand@users.noreply.github.com>
2026-03-29 16:39:12 +1100

    create deployment

---
+++ modules/eks-cluster/provider.tf
@@ -0,0 +1,9 @@
+data "aws_eks_cluster_auth" "this" {
+  name = aws_eks_cluster.eks_cluster.id
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = aws_eks_cluster.eks_cluster.certificate_authority.data
+  token                  = data.aws_eks_cluster_auth.this.token
+}
