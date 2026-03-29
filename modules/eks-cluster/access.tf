b33c9d502ea1586ab19c8da09efb8e89ebce4596
Karl Ferdinand <28285671+karl-ferdinand@users.noreply.github.com>
2026-03-29 14:56:27 +1100

    add eks access entry

---
+++ modules/eks-cluster/access.tf
@@ -0,0 +1,15 @@
+resource "aws_eks_access_entry" "access_entry" {
+  cluster_name  = aws_eks_cluster.eks_cluster.id
+  principal_arn = var.iam_principal
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "access_policy" {
+  cluster_name  = aws_eks_cluster.eks_cluster.id
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = aws_eks_access_entry.access_entry.principal_arn
+
+  access_scope {
+    type = "cluster"
+  }
+}
