d9fe2eeeafcb736e69e4973b91a5a115539049a9
Karl Ferdinand <28285671+karl-ferdinand@users.noreply.github.com>
2026-03-29 16:53:07 +1100

    fix errors

--- modules/eks-cluster/provider.tf
+++ modules/eks-cluster/provider.tf
@@ -4,6 +4,6 @@ data "aws_eks_cluster_auth" "this" {
 
 provider "kubernetes" {
   host                   = aws_eks_cluster.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority.data)
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority.data[0])
   token                  = data.aws_eks_cluster_auth.this.token
 }
