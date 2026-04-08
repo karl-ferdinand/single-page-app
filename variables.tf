f7fffef62b1054e1d9ab03dc2f108f2faa0fc625
Karl Ferdinand <28285671+karl-ferdinand@users.noreply.github.com>
2026-04-08 13:44:01 +1000

    clean up

--- variables.tf
+++ variables.tf
@@ -27,39 +27,3 @@ variable "notification_email" {
   type        = string
   sensitive   = true
 }
-
-variable "eks_cluster_name" {
-  description = "Name of EKS cluster"
-  type        = string
-  default     = "inflate-cluster"
-}
-
-variable "subnet_id_a" {
-  description = "ID of subnet A"
-  type        = string
-  sensitive   = true
-}
-
-variable "subnet_id_b" {
-  description = "ID of subnet B"
-  type        = string
-  sensitive   = true
-}
-
-variable "subnet_id_c" {
-  description = "ID of subnet C"
-  type        = string
-  sensitive   = true
-}
-
-variable "iam_principal" {
-  description = "ARN of IAM principal to access EKS cluster"
-  type        = string
-  sensitive   = true
-}
-
-variable "deploy_principal" {
-  description = "ARN of IAM principal to deploy to EKS cluster"
-  type        = string
-  sensitive   = true
-}
