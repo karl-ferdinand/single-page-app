4824f4caa6ed13e268b925ec7914033bb1b7b4a0
Karl Ferdinand <28285671+karl-ferdinand@users.noreply.github.com>
2026-04-08 13:40:22 +1000

    clean up

--- main.tf
+++ main.tf
@@ -31,13 +31,3 @@ module "single_page_app" {
   topic_name         = var.topic_name
   notification_email = var.notification_email
 }
-
-# module "eks_cluster" {
-#   source           = "./modules/eks-cluster"
-#   eks_cluster_name = var.eks_cluster_name
-#   subnet_id_a      = var.subnet_id_a
-#   subnet_id_b      = var.subnet_id_b
-#   subnet_id_c      = var.subnet_id_c
-#   iam_principal    = var.iam_principal
-#   deploy_principal = var.deploy_principal
-# }
--- modules/eks-cluster/provider.tf
+++
@@ -1,9 +0,0 @@
-data "aws_eks_cluster_auth" "this" {
-  name = aws_eks_cluster.eks_cluster.id
-}
-
-provider "kubernetes" {
-  host                   = aws_eks_cluster.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
-}
